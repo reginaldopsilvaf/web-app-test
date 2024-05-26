3dce8c96d375d0d0acd07f78ef3767d999b52634
reginaldopsilvaf <reginaldopsf@outlook.com>
2024-05-26 01:01:14 -0300

    Conectando o banco de dados

--- fiocruz.py
+++ fiocruz.py
@@ -4,14 +4,14 @@ from database import db
 from flask_migrate import Migrate
 
 if __name__ == '__main__':
-    #db.init_app(app)
-    #conexao = 'postgresql://meubanco.sql'
+    db.init_app(app)
+    conexao = 'postgresql://meubanco.sql'
 
-    #app.config['SECRET_KEY'] = "palavra-secreta"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = conexao
-    #app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = "palavra-secreta"
+    app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+    app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
 
-    #migrate = Migrate(app, db)
+    migrate = Migrate(app, db)
 
     port = int(os.getenv('PORT', default='5000'))
     app.run(host='0.0.0.0', port=port)
